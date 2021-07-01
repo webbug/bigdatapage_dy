4122b8ef4a40bd3b91465a525309b1c104c069a1
xiangsongtao <1225747849@qq.com>
2021-07-01 13:53:04 +0800

    消防安全报告纵坐标颜色改变

--- bigdataStyle1/js/echarts-tool.js
+++ bigdataStyle1/js/echarts-tool.js
@@ -42,14 +42,14 @@ $(document).ready(function() {
 
 	//高发时段
 	//gfsd();
-	
+
 	//类型占比
 	questionFn();
-	
+
 	//进度条
 	BarInit();
 	BarInit2();
-	
+
 	//维保仪表
 	// wbyb();
 
@@ -57,278 +57,324 @@ $(document).ready(function() {
 
 
 //维保仪表
-function wbyb(){
+function wbyb() {
 
 
-    var myMap=echarts.init(document.querySelector('#webyb'))
+	var myMap = echarts.init(document.querySelector('#webyb'))
 
-    var option = {
-      series:[
-        {
-          type:'gauge',
-          data:[{value:97,name:'速度'},{value:10,itemStyle:{color:'purple'},}],
-          splitNumber:5,  //仪表盘刻度的分割段数。
-          min:5, //配置仪表板的数值范围
-          max:110
-          
-        }
-      ],
+	var option = {
+		series: [{
+			type: 'gauge',
+			data: [{
+				value: 97,
+				name: '速度'
+			}, {
+				value: 10,
+				itemStyle: {
+					color: 'purple'
+				},
+			}],
+			splitNumber: 5, //仪表盘刻度的分割段数。
+			min: 5, //配置仪表板的数值范围
+			max: 110
+
+		}],
 
 
-    };
+	};
 
-    myMap.setOption(option);
-window.addEventListener("resize", function() {
-          	myMap.resize()
-          })
+	myMap.setOption(option);
+	window.addEventListener("resize", function() {
+		myMap.resize()
+	})
 
-  }
+}
 
 
 //生成进度条
 function BarInit() {
-          var BarInit = echarts.init(document.getElementById('echartsBar'));// 初始化echarts实例
- 
-          var option={
-              grid: {   // 直角坐标系内绘图网格
-                left: '0',  //grid 组件离容器左侧的距离,
-                             //left的值可以是80这样具体像素值，
-                            //也可以是'80%'这样相对于容器高度的百分比
-                top: '0',
-                right: '0',
-                bottom: '0',
-                containLabel: true   //gid区域是否包含坐标轴的刻度标签。为true的时候，
-                // left/right/top/bottom/width/height决定的是包括了坐标轴标签在内的
-                //所有内容所形成的矩形的位置.常用于【防止标签溢出】的场景
-              },
-              xAxis: {  //直角坐标系grid中的x轴,
-                        //一般情况下单个grid组件最多只能放上下两个x轴,
-                        //多于两个x轴需要通过配置offset属性防止同个位置多个x轴的重叠。
-                type: 'value',//坐标轴类型,分别有：
-                              //'value'-数值轴；'category'-类目轴;
-                              //'time'-时间轴;'log'-对数轴
-                splitLine: {show: false},//坐标轴在 grid 区域中的分隔线
-                axisLabel: {show: false},//坐标轴刻度标签
-                axisTick: {show: false},//坐标轴刻度
-                axisLine: {show: false},//坐标轴轴线
-              },
-              yAxis: {
-                type: 'category',
-                axisTick: {show: false},
-                axisLine: {show: false},
-                axisLabel: {
-                  color: '#fff',
-                  fontSize: 12
-                },
-                data: ['完成率','待处理','已完成']//类目数据，在类目轴（type: 'category'）中有效。
-                       //如果没有设置 type，但是设置了axis.data,则认为type 是 'category'。
-              },
-              series: [//系列列表。每个系列通过 type 决定自己的图表类型
-                {
-                  name: '%',//系列名称
-                  type: 'bar',//柱状、条形图
-                  barWidth: 10,//柱条的宽度,默认自适应
-                  data: [80,40,60],//系列中数据内容数组
-                  label: { //图形上的文本标签
-                    show: true,
-                    position: 'right',//标签的位置
-                    offset: [0,-13],  //标签文字的偏移，此处表示向上偏移40
-                    formatter: '{c}{a}',//标签内容格式器 {a}-系列名,{b}-数据名,{c}-数据值
-                    color: function(params) {//标签字体颜色
-				  			var colorList = ["#edcd44", "#1cadbd", "#1cadbd"];
-						    return colorList[params.dataIndex]
+	var BarInit = echarts.init(document.getElementById('echartsBar')); // 初始化echarts实例
+
+	var option = {
+		grid: { // 直角坐标系内绘图网格
+			left: '0', //grid 组件离容器左侧的距离,
+			//left的值可以是80这样具体像素值，
+			//也可以是'80%'这样相对于容器高度的百分比
+			top: '0',
+			right: '0',
+			bottom: '0',
+			containLabel: true //gid区域是否包含坐标轴的刻度标签。为true的时候，
+			// left/right/top/bottom/width/height决定的是包括了坐标轴标签在内的
+			//所有内容所形成的矩形的位置.常用于【防止标签溢出】的场景
+		},
+		xAxis: { //直角坐标系grid中的x轴,
+			//一般情况下单个grid组件最多只能放上下两个x轴,
+			//多于两个x轴需要通过配置offset属性防止同个位置多个x轴的重叠。
+			type: 'value', //坐标轴类型,分别有：
+			//'value'-数值轴；'category'-类目轴;
+			//'time'-时间轴;'log'-对数轴
+			splitLine: {
+				show: false
+			}, //坐标轴在 grid 区域中的分隔线
+			axisLabel: {
+				show: false
+			}, //坐标轴刻度标签
+			axisTick: {
+				show: false
+			}, //坐标轴刻度
+			axisLine: {
+				show: false
+			}, //坐标轴轴线
+		},
+		yAxis: {
+			type: 'category',
+			axisTick: {
+				show: false
+			},
+			axisLine: {
+				show: false
+			},
+			axisLabel: {
+				color: '#fff',
+				fontSize: 12
+			},
+			data: ['完成率', '待处理', '已完成'] //类目数据，在类目轴（type: 'category'）中有效。
+			//如果没有设置 type，但是设置了axis.data,则认为type 是 'category'。
+		},
+		series: [ //系列列表。每个系列通过 type 决定自己的图表类型
+			{
+				name: '%', //系列名称
+				type: 'bar', //柱状、条形图
+				barWidth: 10, //柱条的宽度,默认自适应
+				data: [80, 40, 60], //系列中数据内容数组
+				label: { //图形上的文本标签
+					show: true,
+					position: 'right', //标签的位置
+					offset: [0, -13], //标签文字的偏移，此处表示向上偏移40
+					formatter: '{c}{a}', //标签内容格式器 {a}-系列名,{b}-数据名,{c}-数据值
+					color: function(params) { //标签字体颜色
+						var colorList = ["#edcd44", "#1cadbd", "#1cadbd"];
+						return colorList[params.dataIndex]
 					},
-                    fontSize: 12  //标签字号
-                  },
-				  itemStyle: {
-				  	normal: {
-						barBorderRadius: 10,//柱条圆角半径,单位px.
-				  		color: function(params) {
-				  			var colorList = ["#edcd44", "#1cadbd", "#1cadbd"];
-				  			return colorList[params.dataIndex]
-				  		},
-				  	}
-				  },
-				  zlevel:1//柱状图所有图形的 zlevel 值,
-                          //zlevel 大的 Canvas 会放在 zlevel 小的 Canvas 的上面
-                },
-                {
-                  name: '进度条背景',
-                  type: 'bar',
-                  barGap: '-100%',//不同系列的柱间距离，为百分比。
-                  // 在同一坐标系上，此属性会被多个 'bar' 系列共享。
-                  // 此属性应设置于此坐标系中最后一个 'bar' 系列上才会生效，
-                   //并且是对此坐标系中所有 'bar' 系列生效。
-                  barWidth: 10,
-                  data: [100, 100, 100],
-                  color: '#fff',//柱条颜色
-                  itemStyle: {
-                    normal: {
-                      barBorderRadius: 10
-                    }
-                  }
-                }
-              ]
-            }
-          BarInit.setOption(option);
-          window.addEventListener("resize", function() {
-          	BarInit.resize()
-          })
-}
-function BarInit2() {
-          var BarInit = echarts.init(document.getElementById('echartsBar2'));// 初始化echarts实例
- 
-          var option={
-              grid: {   // 直角坐标系内绘图网格
-                left: '0',  //grid 组件离容器左侧的距离,
-                             //left的值可以是80这样具体像素值，
-                            //也可以是'80%'这样相对于容器高度的百分比
-                top: '0',
-                right: '0',
-                bottom: '0',
-                containLabel: true   //gid区域是否包含坐标轴的刻度标签。为true的时候，
-                // left/right/top/bottom/width/height决定的是包括了坐标轴标签在内的
-                //所有内容所形成的矩形的位置.常用于【防止标签溢出】的场景
-              },
-              xAxis: {  //直角坐标系grid中的x轴,
-                        //一般情况下单个grid组件最多只能放上下两个x轴,
-                        //多于两个x轴需要通过配置offset属性防止同个位置多个x轴的重叠。
-                type: 'value',//坐标轴类型,分别有：
-                              //'value'-数值轴；'category'-类目轴;
-                              //'time'-时间轴;'log'-对数轴
-                splitLine: {show: false},//坐标轴在 grid 区域中的分隔线
-                axisLabel: {show: false},//坐标轴刻度标签
-                axisTick: {show: false},//坐标轴刻度
-                axisLine: {show: false},//坐标轴轴线
-              },
-              yAxis: {
-                type: 'category',
-                axisTick: {show: false},
-                axisLine: {show: false},
-                axisLabel: {
-                  color: '#fff',
-                  fontSize: 12
-                },
-                data: ['完成率','待处理','已完成']//类目数据，在类目轴（type: 'category'）中有效。
-                       //如果没有设置 type，但是设置了axis.data,则认为type 是 'category'。
-              },
-              series: [//系列列表。每个系列通过 type 决定自己的图表类型
-                {
-                  name: '%',//系列名称
-                  type: 'bar',//柱状、条形图
-                  barWidth: 10,//柱条的宽度,默认自适应
-                  data: [80,40,60],//系列中数据内容数组
-                  label: { //图形上的文本标签
-                    show: true,
-                    position: 'right',//标签的位置
-                    offset: [0,-13],  //标签文字的偏移，此处表示向上偏移40
-                    formatter: '{c}{a}',//标签内容格式器 {a}-系列名,{b}-数据名,{c}-数据值
-                    color: function(params) {//标签字体颜色
-				  			var colorList = ["#edcd44", "#1cadbd", "#1cadbd"];
-						    return colorList[params.dataIndex]
-					},
-                    fontSize: 12  //标签字号
-                  },
-				  itemStyle: {
-				  	normal: {
-						barBorderRadius: 10,//柱条圆角半径,单位px.
-				  		color: function(params) {
-				  			var colorList = ["#edcd44", "#1cadbd", "#1cadbd"];
-				  			return colorList[params.dataIndex]
-				  		},
-				  	}
-				  },
-				  zlevel:1//柱状图所有图形的 zlevel 值,
-                          //zlevel 大的 Canvas 会放在 zlevel 小的 Canvas 的上面
-                },
-                {
-                  name: '进度条背景',
-                  type: 'bar',
-                  barGap: '-100%',//不同系列的柱间距离，为百分比。
-                  // 在同一坐标系上，此属性会被多个 'bar' 系列共享。
-                  // 此属性应设置于此坐标系中最后一个 'bar' 系列上才会生效，
-                   //并且是对此坐标系中所有 'bar' 系列生效。
-                  barWidth: 10,
-                  data: [100, 100, 100],
-                  color: '#fff',//柱条颜色
-                  itemStyle: {
-                    normal: {
-                      barBorderRadius: 10
-                    }
-                  }
-                }
-              ]
-            }
-          BarInit.setOption(option);
-          window.addEventListener("resize", function() {
-          	BarInit.resize()
-          })
+					fontSize: 12 //标签字号
+				},
+				itemStyle: {
+					normal: {
+						barBorderRadius: 10, //柱条圆角半径,单位px.
+						color: function(params) {
+							var colorList = ["#edcd44", "#1cadbd", "#1cadbd"];
+							return colorList[params.dataIndex]
+						},
+					}
+				},
+				zlevel: 1 //柱状图所有图形的 zlevel 值,
+				//zlevel 大的 Canvas 会放在 zlevel 小的 Canvas 的上面
+			},
+			{
+				name: '进度条背景',
+				type: 'bar',
+				barGap: '-100%', //不同系列的柱间距离，为百分比。
+				// 在同一坐标系上，此属性会被多个 'bar' 系列共享。
+				// 此属性应设置于此坐标系中最后一个 'bar' 系列上才会生效，
+				//并且是对此坐标系中所有 'bar' 系列生效。
+				barWidth: 10,
+				data: [100, 100, 100],
+				color: '#fff', //柱条颜色
+				itemStyle: {
+					normal: {
+						barBorderRadius: 10
+					}
+				}
+			}
+		]
+	}
+	BarInit.setOption(option);
+	window.addEventListener("resize", function() {
+		BarInit.resize()
+	})
 }
 
-  // 问题反馈类型占比
-            function questionFn() {
-                var numArr = [
-                    {name: '高发时段1', value: 300},
-                    {name: '高发时段2', value: 500},
-                    {name: '高发时段3', value: 400},
-                    {name: '高发时段4', value: 350},
-                    {name: '高发时段5', value: 363},
-                    {name: '高发时段6', value: 800},
-                ]
-                // for(var i = 0; i < data.list.length; i++) {
-                //     numArr.push({name: data.list[i].name,value: data.list[i].num})
-                // }
-                var datas = numArr
-                var questionChart = echarts.init(document.getElementById('questionId'));
-                var option = {
-                    title: {
-                        left: 'center'
-                    },
-                    tooltip: {
-                        trigger: 'item',
-                        formatter: '{a} <br/>{b} : {c} ({d}%)'
-                    },
-                    color:['#1a9eac','#37D2D4', '#d8bb3e', '#a0c52b', '#c57127'],
-                    legend: {
-                        itemWidth: 11,// 标志图形的长度
-                        itemHeight: 11,// 标志图形的宽度
-                        orient: 'vertical',
-                        // left: 'right',
-                        top: '15%',
-                        x: '42%',
-                        data: datas,
-                        textStyle: { //图例文字的样式
-                            color: '#fff',
-                            fontSize: 8
-                        },
-                    },
-                    series: [
-                        {
-                            name: '问题反馈',
-                            type: 'pie',
-                            radius: '75%',
-                            center: ['20%', '50%'],
-                            animationDuration: 2500,
-                            data: datas,
-                            label: {
-                                normal: {
-                                    position: 'inner',
-                                    show : false
-                                }
-                            }
-                        }
-                    ]
-                };
-                questionChart.setOption(option)
-                setInterval(function() {
-                    questionChart.clear()
-                    questionChart.setOption(option)
-                },8000)
-				window.addEventListener("resize", function() {
-					questionChart.resize()
-				})
-            }
-            
+function BarInit2() {
+	var BarInit = echarts.init(document.getElementById('echartsBar2')); // 初始化echarts实例
+
+	var option = {
+		grid: { // 直角坐标系内绘图网格
+			left: '0', //grid 组件离容器左侧的距离,
+			//left的值可以是80这样具体像素值，
+			//也可以是'80%'这样相对于容器高度的百分比
+			top: '0',
+			right: '0',
+			bottom: '0',
+			containLabel: true //gid区域是否包含坐标轴的刻度标签。为true的时候，
+			// left/right/top/bottom/width/height决定的是包括了坐标轴标签在内的
+			//所有内容所形成的矩形的位置.常用于【防止标签溢出】的场景
+		},
+		xAxis: { //直角坐标系grid中的x轴,
+			//一般情况下单个grid组件最多只能放上下两个x轴,
+			//多于两个x轴需要通过配置offset属性防止同个位置多个x轴的重叠。
+			type: 'value', //坐标轴类型,分别有：
+			//'value'-数值轴；'category'-类目轴;
+			//'time'-时间轴;'log'-对数轴
+			splitLine: {
+				show: false
+			}, //坐标轴在 grid 区域中的分隔线
+			axisLabel: {
+				show: false
+			}, //坐标轴刻度标签
+			axisTick: {
+				show: false
+			}, //坐标轴刻度
+			axisLine: {
+				show: false
+			}, //坐标轴轴线
+		},
+		yAxis: {
+			type: 'category',
+			axisTick: {
+				show: false
+			},
+			axisLine: {
+				show: false
+			},
+			axisLabel: {
+				color: '#fff',
+				fontSize: 12
+			},
+			data: ['完成率', '待处理', '已完成'] //类目数据，在类目轴（type: 'category'）中有效。
+			//如果没有设置 type，但是设置了axis.data,则认为type 是 'category'。
+		},
+		series: [ //系列列表。每个系列通过 type 决定自己的图表类型
+			{
+				name: '%', //系列名称
+				type: 'bar', //柱状、条形图
+				barWidth: 10, //柱条的宽度,默认自适应
+				data: [80, 40, 60], //系列中数据内容数组
+				label: { //图形上的文本标签
+					show: true,
+					position: 'right', //标签的位置
+					offset: [0, -13], //标签文字的偏移，此处表示向上偏移40
+					formatter: '{c}{a}', //标签内容格式器 {a}-系列名,{b}-数据名,{c}-数据值
+					color: function(params) { //标签字体颜色
+						var colorList = ["#edcd44", "#1cadbd", "#1cadbd"];
+						return colorList[params.dataIndex]
+					},
+					fontSize: 12 //标签字号
+				},
+				itemStyle: {
+					normal: {
+						barBorderRadius: 10, //柱条圆角半径,单位px.
+						color: function(params) {
+							var colorList = ["#edcd44", "#1cadbd", "#1cadbd"];
+							return colorList[params.dataIndex]
+						},
+					}
+				},
+				zlevel: 1 //柱状图所有图形的 zlevel 值,
+				//zlevel 大的 Canvas 会放在 zlevel 小的 Canvas 的上面
+			},
+			{
+				name: '进度条背景',
+				type: 'bar',
+				barGap: '-100%', //不同系列的柱间距离，为百分比。
+				// 在同一坐标系上，此属性会被多个 'bar' 系列共享。
+				// 此属性应设置于此坐标系中最后一个 'bar' 系列上才会生效，
+				//并且是对此坐标系中所有 'bar' 系列生效。
+				barWidth: 10,
+				data: [100, 100, 100],
+				color: '#fff', //柱条颜色
+				itemStyle: {
+					normal: {
+						barBorderRadius: 10
+					}
+				}
+			}
+		]
+	}
+	BarInit.setOption(option);
+	window.addEventListener("resize", function() {
+		BarInit.resize()
+	})
+}
+
+// 问题反馈类型占比
+function questionFn() {
+	var numArr = [{
+			name: '高发时段1',
+			value: 300
+		},
+		{
+			name: '高发时段2',
+			value: 500
+		},
+		{
+			name: '高发时段3',
+			value: 400
+		},
+		{
+			name: '高发时段4',
+			value: 350
+		},
+		{
+			name: '高发时段5',
+			value: 363
+		},
+		{
+			name: '高发时段6',
+			value: 800
+		},
+	]
+	// for(var i = 0; i < data.list.length; i++) {
+	//     numArr.push({name: data.list[i].name,value: data.list[i].num})
+	// }
+	var datas = numArr
+	var questionChart = echarts.init(document.getElementById('questionId'));
+	var option = {
+		title: {
+			left: 'center'
+		},
+		tooltip: {
+			trigger: 'item',
+			formatter: '{a} <br/>{b} : {c} ({d}%)'
+		},
+		color: ['#1a9eac', '#37D2D4', '#d8bb3e', '#a0c52b', '#c57127'],
+		legend: {
+			itemWidth: 11, // 标志图形的长度
+			itemHeight: 11, // 标志图形的宽度
+			orient: 'vertical',
+			// left: 'right',
+			top: '15%',
+			x: '42%',
+			data: datas,
+			textStyle: { //图例文字的样式
+				color: '#fff',
+				fontSize: 8
+			},
+		},
+		series: [{
+			name: '问题反馈',
+			type: 'pie',
+			radius: '75%',
+			center: ['20%', '50%'],
+			animationDuration: 2500,
+			data: datas,
+			label: {
+				normal: {
+					position: 'inner',
+					show: false
+				}
+			}
+		}]
+	};
+	questionChart.setOption(option)
+	setInterval(function() {
+		questionChart.clear()
+		questionChart.setOption(option)
+	}, 8000)
+	window.addEventListener("resize", function() {
+		questionChart.resize()
+	})
+}
+
 
 function wbzl() {
 	var wbzl = echarts.init(document.getElementById('wbzl'));
@@ -538,7 +584,7 @@ function wxyg() {
 				},
 				axisLine: {
 					lineStyle: {
-						color: '#5e859e', //纵坐标轴和字体颜色
+						color: 'rgba(140,156,88,1)', //纵坐标轴和字体颜色
 						width: 2
 					}
 				}
@@ -552,8 +598,8 @@ function wxyg() {
 				data: [90, 80, 70, 68, 65, 64, 60, 57, 68, 60, 64, 10],
 				barWidth: '25%',
 				itemStyle: {
-					normal:{
-						barBorderRadius: [5,5,0,0]
+					normal: {
+						barBorderRadius: [5, 5, 0, 0]
 					}
 				}
 
@@ -573,8 +619,8 @@ function wxyg() {
 				data: [70, 68, 65, 64, 60, 57, 68, 65, 64, 60, 70, 70],
 				barWidth: '25%',
 				itemStyle: {
-					normal:{
-						barBorderRadius: [5,5,0,0]
+					normal: {
+						barBorderRadius: [5, 5, 0, 0]
 					}
 				}
 			},
@@ -593,8 +639,8 @@ function wxyg() {
 				data: [300, 270, 180, 350, 140, 300, 70, 50, 40, 38, 30, 90],
 				barWidth: '25%',
 				itemStyle: {
-					normal:{
-						barBorderRadius: [5,5,0,0]
+					normal: {
+						barBorderRadius: [5, 5, 0, 0]
 					}
 				}
 			},
